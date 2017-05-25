a520b7e639f6d7f4db329b5361eb539bb28ce943
15004557 <H0peyur3gd7>
2017-05-25 11:47:37 +0800

    With sound & notification priority = high

--- app/src/main/java/demonotification/android/myapplicationdev/com/demonotification/MainActivity.java
+++ app/src/main/java/demonotification/android/myapplicationdev/com/demonotification/MainActivity.java
@@ -1,13 +1,65 @@
 package demonotification.android.myapplicationdev.com.demonotification;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    int requestCode = 123;
+    int notificationID = 888;
+
+    Button btnNotify1, btnNotify2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNotify1 = (Button)findViewById(R.id.btnNotify1);
+
+        btnNotify2 = (Button)findViewById(R.id.btnNotify2);
+
+        btnNotify1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+
+                PendingIntent pIntent = PendingIntent.getActivity(MainActivity.this, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                //build notification
+                Notification.Builder builder = new Notification.Builder(MainActivity.this);
+                builder.setContentTitle("Amazing Offer!");
+                builder.setSmallIcon(R.mipmap.ic_launcher);
+                builder.setContentIntent(pIntent);
+                builder.setAutoCancel(true);
+
+
+                Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                builder.setSound(uri);
+
+                builder.setPriority(Notification.PRIORITY_HIGH);
+
+                Notification n = builder.build();
+                NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+                //a good to have id, in case you want to cancel it programmatically later on
+                notificationManager.notify(notificationID, n);
+                finish();
+
+
+
+            }
+        });
+
+
+
     }
 }
