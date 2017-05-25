61546c424918257971cfd452c9c4930076430fd1
15004557 <H0peyur3gd7>
2017-05-25 10:41:33 +0800

    Initial commit

---
+++ app/src/main/java/demonotification/android/myapplicationdev/com/demonotification/MainActivity.java
@@ -0,0 +1,13 @@
+package demonotification.android.myapplicationdev.com.demonotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
